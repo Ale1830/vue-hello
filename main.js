9245b0d17ffd01117937c9c9a7e3e8296c32c4e1
Alessio Tiberi <alessio.1997.18.01@gmail.com>
2022-01-14 03:44:52 +0100

    h1 + bonus

---
+++ main.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+      message: '2-1 A CASA',
+      image:'https://cdn.tuttosport.com/images/2022/01/12/234757966-b09fb764-04b6-4f16-8d27-3fc408d760f7.jpg'
+      
+    }
+  })
